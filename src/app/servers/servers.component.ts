18227f8e189604151db879acc37c58196081e1b0
Cernovschi Vasilica <vasilica.cernovschi@student.usv.ro>
2023-01-21 15:02:27 +0200

    event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,12 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
+
 export class ServersComponent {
   allowNewServer: boolean = true;
+  serverCreationStatus: string = "No server was created";
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = false;
     }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server was created";
   }
 }
