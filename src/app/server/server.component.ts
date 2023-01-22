65e3a6bd2d13e99bbcd249b6910251ce4e760daa
Cernovschi Vasilica <vasilica.cernovschi@student.usv.ro>
2023-01-22 13:23:42 +0200

    ngClass and ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,9 +2,18 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [` 
+    .online {
+      color: white;
+    }
+  `]
 }) 
 export class ServerComponent{
   serverId: number = 20;
   serverStatus: string = "offline";
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online': 'offline';
+  }
 }
